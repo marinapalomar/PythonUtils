3a534ec3ec2f239d04290b67b51d86072449033b
Marina <marina.palomar@valerdat.com>
2023-07-17 11:21:40 +0200

    Update df_Utils.py

--- Utils/df_Utils.py
+++ Utils/df_Utils.py
@@ -6,7 +6,6 @@ Created on Tue Jul  4 15:09:51 2023
 """
 import pandas as pd
 import missingno as msno
-import matplotlib.pyplot as plt
 from IPython.display import display
 
 
@@ -14,27 +13,32 @@ from IPython.display import display
 def read_df(df_path):
     return pd.read_csv(df_path)
 
+def count_null_values(df):
+    print(pd.isnull(df).values.sum(),'entradas nulas,'\
+          ' correspondientes a un {:.2f}% de los valores en el df'.format(pd.isnull(df).values.sum()*100/df.size))
+    
+def count_duplicate_rows(df):
+    n = df.duplicated().sum()
+    print('Número de filas duplicadas: ', n)
+    return n
+    
+def delete_duplicate_rows(df):
+    df.drop_duplicates(subset=None,keep="first",inplace=True)
+    return df
+    
+def plot_null_matrix(df):
+    return msno.matrix(df,figsize=(5,4),fontsize=10)
+
 # Funció per fer una vista ràpida d'un df. Mira els valors null i les entrades
 # duplicades. Per defecte adjunta un plot per visualitzar on estan localitzats
 # els nulls (per canviar-ho entrar do_plot=False) i si la funció s'iguala a una
-# variable, aquesta contindra el df entrat sense valors repetits (si en té).
-def visualitzacio_df(df, do_plot=True, return_df = False):
-    display(df.head())
-    
-    print("\n**Número de NaNs**")
-    print(pd.isnull(df).values.sum(),'entrades nul·les')
-    print('{:.2f}%'.format(pd.isnull(df).values.sum()/df.size*100))
-    if (do_plot):
-        msno.matrix(df,figsize=(5,4),fontsize=10)
-        plt.show()
-    
-    print("\n**Número dades duplicades**")
-    df_copy = df.copy()
-    df_copy.drop_duplicates(subset=None,keep="first",inplace=True)
-    print('Mida original: ',df.shape,
-         '\nNº de dades repetides: ', df.duplicated().sum(),
-         '\nMida un cop fet el drop: ',df_copy.shape)
-    if return_df:
-        return df_copy
-    del df_copy
+# variable, aquesta contindra el df entrat sense valors repetits (si en té).    
+def visualitzacio_df(df):
+    print('Columns:',list(df.columns))
+    display(df)
+    count_null_values(df)
+    plot_null_matrix(df)
+    n = count_duplicate_rows(df)
+    if n != 0:
+        delete_duplicate_rows(df)
     
